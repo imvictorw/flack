7d011a9ee6607f756936ec324e6870577246d95f
imvictorwoo <baseballvictorr@gmail.com>
2019-06-12 17:58:13 -0500

    v1
    Added chatbox functionality.
    
    -Enters a name and message and pops up on textbox
    -Cannot do it twice since it hasn’t stored its name

--- application.py
+++ application.py
@@ -1,13 +1,28 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
+    # return "Project 2: TODO"
+    
+@app.route("/channel", methods=["POST"])
+def channel():
+    name = request.form.get("name")
+    print("1  :" + name)
+    return render_template("channel.html", name=name)
+    
+@app.route("/chatpost", methods=["POST"])
+def chatpost():
+    name = request.form.get("username")
+    message = request.form.get("message")
+    outputM = name + ": " + message
+    print(name)
+    print(message)
+    return render_template("channel.html", outputM = outputM)
